143fa8dfb5065f375248717eb3c8e746968931c3
Giorgio Mandolini <g.mandolini@gmail.com>
2025-07-30 12:15:44 +0200

    feat: added bootstrap italia

---
+++ astro.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "astro/config";
+import { resolve } from "path";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        "@splidejs/splide/src/css/core/index": resolve(
+          "node_modules/@splidejs/splide/src/css/core/index.scss",
+        ),
+      },
+    },
+  },
+});
