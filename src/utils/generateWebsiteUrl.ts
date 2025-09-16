fdd7a0bdcd9ea155c7078c395110ee36e797ac48
mariateresagraziano <marytex.dev@gmail.com>
2025-09-16 10:18:55 +0200

    fix: schema type for localization

--- src/utils/generateWebsiteUrl.ts
+++ src/utils/generateWebsiteUrl.ts
@@ -1,12 +1,24 @@
 import type { SchemaTypes } from "@datocms/cma-client";
+import type { SiteLocale } from "../graphql/types";
+
+type LocalizedSchemaTypesItem = SchemaTypes.Item & {
+  attributes: SchemaTypes.Item["attributes"] & {
+    slug: Record<SiteLocale, string>;
+  };
+};
 
 export function generateWebsiteUrl(
-  item: SchemaTypes.Item,
-  locale: string,
+  item: LocalizedSchemaTypesItem,
+  locale: SiteLocale,
 ): string | null {
   if (!item || !locale) {
     return null;
   }
-  const slug = (item.attributes.slug as Record<string, string>)[locale];
+
+  if (typeof item.attributes.slug !== "object") {
+    throw new Error("Item slug is not localized");
+  }
+
+  const slug = item.attributes.slug[locale];
   return `/${locale}/${slug}`;
 }
