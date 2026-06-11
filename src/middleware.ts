62fe0ed0d971b43c2d53030f7b175567de423e26
Lorezz <point.point@gmail.com>
2026-06-11 11:02:48 +0200

    fix: test middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,32 +1,34 @@
 import { defineMiddleware } from "astro/middleware";
 
 export const onRequest = defineMiddleware((context, next) => {
-  // const { lang } = context.params;
-  // if (lang) {
-  //   context.locals.lang = lang;
-  // }
+  // if (expectedUser && expectedPassword) {
 
-  const expectedUser = import.meta.env.BASIC_AUTH_USER;
-  const expectedPassword = import.meta.env.BASIC_AUTH_PASSWORD;
+  const authHeader = context.request.headers.get("authorization");
 
-  if (expectedUser && expectedPassword) {
-    const authHeader = context.request.headers.get("authorization");
-    if (authHeader) {
-      const authValue = authHeader.split(" ")[1];
-      const decodedAuth = atob(authValue);
-      const [user, password] = decodedAuth.split(":");
-      if (user === expectedUser && password === expectedPassword) {
-        return next();
-      }
+  if (authHeader) {
+    const authValue = authHeader.split(" ")[1];
+    const decodedAuth = atob(authValue);
+    const [user, password] = decodedAuth.split(":");
+
+    if (
+      user === import.meta.env.BASIC_AUTH_USER &&
+      password === import.meta.env.BASIC_AUTH_PASSWORD
+    ) {
+      // const { lang } = context.params;
+      // if (lang) {
+      //   context.locals.lang = lang;
+      // }
+      return next(); // Credentials match, let them in
     }
-
-    return new Response("Auth Required", {
-      status: 401,
-      headers: {
-        "WWW-Authenticate": 'Basic realm="Secure Staging Area"',
-      },
-    });
-  } else {
-    return next();
   }
+
+  // If unauthorized or missing headers, prompt the user for password
+  return new Response("Auth Required", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": 'Basic realm="Secure Staging Area"',
+    },
+  });
+
+  // }
 });
