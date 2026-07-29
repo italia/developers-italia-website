02b511bb8c8e811a53de39dbd034e938613e4428
Andrea Stagi <stagi.andrea@gmail.com>
2026-07-29 15:18:20 +0200

    feat: new redirect with software ids

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,43 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel";
 import { defineConfig } from "astro/config";
+import { readFileSync } from "fs";
 import { resolve } from "path";
+
+const softwareIdsFilePath = "./software-ids.txt";
+let softwareIds: string[] = [];
+
+try {
+  const fileContent = readFileSync(softwareIdsFilePath, "utf-8");
+  softwareIds = fileContent
+    .split("\n")
+    .map((id) => id.trim())
+    .filter(Boolean);
+} catch (error) {
+  console.error(
+    `Error reading software IDs from ${softwareIdsFilePath}:`,
+    error,
+  );
+}
+
+const softwareRedirects = softwareIds.reduce(
+  (redirects, id) => {
+    redirects[`/software/${id}`] = {
+      status: 301 as 301,
+      destination: `https://catalogo-software.developers.italia.it/software/${id}`,
+    };
+    return redirects;
+  },
+  {} as Record<
+    string,
+    { status: 301 | 302 | 303 | 307 | 308 | 300 | 304; destination: string }
+  >,
+);
+
+softwareRedirects["/"] = {
+  status: 301,
+  destination: "/it",
+};
 
 // https://astro.build/config
 export default defineConfig({
@@ -31,16 +67,7 @@ export default defineConfig({
     react(),
   ],
   adapter: vercel(),
-  redirects: {
-    "/": {
-      status: 301,
-      destination: "/it",
-    },
-    "/provaredirect": {
-      status: 301,
-      destination: "https://catalogo-software.developers.italia.it/software/" 
-    }
-  },
+  redirects: softwareRedirects,
   vite: {
     ssr: {
       noExternal: ["graph-italia-components"],
